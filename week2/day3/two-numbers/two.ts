e8fcb5cc85fd43eb4dee6cd1fffb1bc65134a8fa
veszelijani <nopent@citromail.hu>
2020-03-07 10:50:27 +0100

    finished the two-numbers task

---
+++ week2/day3/two-numbers/two.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+let i: number = 22;
+let k: number = 13;
+
+console.log(`the result is: ${i + k}`);
+console.log(`the result is: ${i - k}`);
+console.log(`the result is: ${i * k}`);
+console.log(`the result is: ${i / k}`);
+console.log(`the result is: ${i % k}`);
