4d28bc52bfa2be968744aafe16ab77e1f5d52591
veszelijani <nopent@citromail.hu>
2020-03-07 12:40:01 +0100

    finished some loop exercise

---
+++ week2/day3/Loop exercises/iwont.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+let iWontCheat: string = "I won't cheat on the exam";
+
+for (let i: number = 1; i <= 100; i++) {
+    console.log(i + ' ' + iWontCheat);
+}
+
+for (let k: number = 0; k <= 500; k++) {
+    console.log(k)
+}
+
+let number: number = 15;
+
+for (let z: number = 1; z <= 10; z++){
+    console.log(`${z} * ${number} = ${z * number} `)
+}
