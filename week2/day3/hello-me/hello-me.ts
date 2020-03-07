1147dac2254caece50d15d168f96bec266a9bd83
veszelijani <nopent@citromail.hu>
2020-03-07 10:38:10 +0100

    finished the first and the sec task

---
+++ week2/day3/hello-me/hello-me.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+let myName: string = 'János';
+
+console.log(`Hello, ${myName}!`);
+
+
+// Modify this program to console.log Humpty Dumpty riddle correctly
+
+
+let first = 'All the king\'s horses and all the king\'s men';
+
+let second = 'Humpty Dumpty had a great fall.';
+
+let third = 'Humpty Dumpty sat on a wall,';
+
+let last = 'Couldn\'t put Humpty together again.';
+
+console.log(third,second,first,last)
