7296e5061c660f615c9bd19a947abb960f6fd433
veszelijani <nopent@citromail.hu>
2020-03-07 16:46:11 +0100

    finished the draw-square exercise

---
+++ week2/day3/Loop exercises/draw-square.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+let lineCount: number = 6;
+
+for (let i: number = 0; i <= lineCount; i++) {
+    let a: string = '';
+    for (let j: number = 0; j <= lineCount; j++) {
+        if (i == 0 || j == 0 || i == lineCount || j == lineCount) {
+            a = a + '%';
+        } else {
+            a = a + ' ';
+        }
+    }
+    console.log(a);
+}
