aa92b32b2d286f6f1577f48fb2bd2a00c8482f91
veszelijani <nopent@citromail.hu>
2020-03-07 13:07:22 +0100

    draw-triangle finished

---
+++ week2/day3/Loop exercises/draw-triangle.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+let lineCount: number = 4;
+let starThing: string = '*';
+
+for(let i: number = 0; i < lineCount; i++){
+    console.log(starThing);
+    starThing = starThing + '*'
+}
