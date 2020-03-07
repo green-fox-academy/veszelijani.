45b16e09673f15b10e5613c53b04767acd37d998
veszelijani <nopent@citromail.hu>
2020-03-07 11:17:46 +0100

    finished some tasks

--- week2/day3/coding-hours/coding-hours.ts
+++ week2/day3/coding-hours/coding-hours.ts
@@ -8,3 +8,17 @@ let averageWeekly: number = 52;
 console.log(`Green Fox attendee codes ${dailyCoding * workdaysCoding * semesterLong} hours in a Semester`);
 
 console.log(`The percentage of the coding hours in the semester: ${(dailyCoding * workdaysCoding) / averageWeekly * 100} %`);
+
+
+let myName: string = 'János';
+let myAge: number = 25;
+let myHeight: number = 1.82;
+let amImarried: boolean = false;
+
+console.log(`My name is ${myName} and I am ${myAge} old. My height is ${myHeight}m`);
+
+if (amImarried == false) {
+    console.log("and I am not married");
+} else {
+    console.log("and I am married");
+}
---
+++ week2/day3/favorite number and swap/fav number and swap.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+let favoriteNumber: number = 8;
+
+console.log('my favorite number is: '+favoriteNumber);
+
+
+let a: number = 123;
+let b: number = 526;
+let c: number;
+
+c = a;
+a = b;
+b = c;
+
+console.log(a);
+console.log(b);
