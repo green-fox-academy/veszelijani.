c29b04aa9fcf41342c8103d2e091380a6fa0593f
veszelijani <nopent@citromail.hu>
2020-04-04 14:41:38 +0200

    added the hunger,thirst,play methods and tested

---
+++ week6/day3/3. Animal/Animal.ts
@@ -0,0 +1,31 @@
+'use strict';
+
+class Animal {
+    private _hunger: number;
+    private _thirst: number;
+
+    constructor() {
+        this._hunger = 50;
+        this._thirst = 50;
+    }
+
+    public eat(): void {
+        this._hunger -= 1;
+
+    }
+    public drink(): void {
+        this._thirst -= 1;
+
+    }
+    public play(): void {
+        this._hunger++;
+        this._thirst++
+    }
+}
+let Tiger = new Animal();
+Tiger.drink()
+Tiger.eat()
+console.log(Tiger)
+Tiger.play()
+console.log(Tiger)
+
