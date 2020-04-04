7fa6d2f84b9997d07d6d856fe06569730ba41d38
veszelijani <nopent@citromail.hu>
2020-04-04 16:46:09 +0200

    UPDATED: Had the same problem in reversed-order.ts but fixed

--- week4/Reversed-order/reversed-order.ts
+++ week4/Reversed-order/reversed-order.ts
@@ -3,7 +3,7 @@
 const fs = require('fs');
 try {
     let fileContent = fs.readFileSync('text.txt', 'utf-8');
-    let newContent: string[] = fileContent.split(' ').reverse().join(' ')
+    let newContent: string[] = fileContent.split('\r\n').reverse().join('\r\n')
     fs.writeFileSync('newText.txt', newContent);
     console.log(newContent)
 } catch (e) {
