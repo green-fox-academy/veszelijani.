44449ea109aeb45638b5d0cc6f0743ff6f283f3c
veszelijani <nopent@citromail.hu>
2020-05-01 18:54:09 +0200

    fixed the loop error

--- week3/day3/bank-transfer.ts
+++ week3/day3/bank-transfer.ts
@@ -18,25 +18,27 @@ function getNameAndBalance(accNumber: number): any[] {
 console.log(getNameAndBalance(23456311));
 
 function transferAmount(clientName: string, fromAccNumber: number, toAccNumber: number, amount: number): any[] {
+    let counter: number = 0
     for (let i: number = 0; i < accounts.length; i++) {
         if (accounts[i].clientName === clientName && accounts[i].accountNumber === fromAccNumber) {
+            counter++
             if (accounts[i].balance >= amount) {
                 accounts[i].balance -= amount;
             } else {
                 console.log('Insufficient money,tho');
-                break;
+                //break;
             }
             for (let j: number = 0; j < accounts.length; j++) {
                 if (accounts[j].accountNumber === toAccNumber) {
                     accounts[j].balance += amount;
                 }
             }
-        } else {
-            console.log("Invalid account");
-            break;
         }
-    } return accounts
+    }if(counter < 1){
+        console.log('ERROR : Invalid account');
+    }
+     return accounts
 }
-transferAmount('Igor', 11234543, 43546731, 2000);
+console.log(transferAmount('Sergei', 23456311, 43546731, 2000));
 
 
