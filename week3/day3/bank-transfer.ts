ab56bb2b1b60f3b2f96cbda0dfcdeb688f1d9a82
veszelijani <nopent@citromail.hu>
2020-05-01 18:41:18 +0200

    fixed the transfer function,added some condition and tested

--- week3/day3/bank-transfer.ts
+++ week3/day3/bank-transfer.ts
@@ -17,26 +17,26 @@ function getNameAndBalance(accNumber: number): any[] {
 }
 console.log(getNameAndBalance(23456311));
 
-function transferAmount(clientName: string, fromAccNumber: number, toAccNumber: number, amount: number):any[]{
-    for(let i: number = 0; i < accounts.length; i++){
-        if(accounts[i].clientName === clientName && accounts[i].accountNumber === fromAccNumber){
-            accounts[i].balance = accounts[i].balance - amount
-            break                
-        }else{
-            console.log('404 account not found');             
+function transferAmount(clientName: string, fromAccNumber: number, toAccNumber: number, amount: number): any[] {
+    for (let i: number = 0; i < accounts.length; i++) {
+        if (accounts[i].clientName === clientName && accounts[i].accountNumber === fromAccNumber) {
+            if (accounts[i].balance >= amount) {
+                accounts[i].balance -= amount;
+            } else {
+                console.log('Insufficient money,tho');
+                break;
+            }
+            for (let j: number = 0; j < accounts.length; j++) {
+                if (accounts[j].accountNumber === toAccNumber) {
+                    accounts[j].balance += amount;
+                }
+            }
+        } else {
+            console.log("Invalid account");
+            break;
         }
-    }
-    for (let j: number = 0; j <accounts.length; j++){
-        if(accounts[j].accountNumber === toAccNumber){
-            accounts[j].balance =  accounts[j].balance + amount
-            break
-        }else{
-            console.log('404 account NOT FOUND');
-            
-        }
-    }
-    return accounts
+    } return accounts
 }
-console.log(transferAmount('Igor', 11234543, 43546731, 2000 ));
+transferAmount('Igor', 11234543, 43546731, 2000);
 
 
