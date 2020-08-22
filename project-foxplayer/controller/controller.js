23149679dff82de58971695be95804647b4b7ccc
veszelijani <nopent@citromail.hu>
2020-08-22 15:34:05 +0200

    fixing the get method

--- project-foxplayer/controller/controller.js
+++ project-foxplayer/controller/controller.js
@@ -8,7 +8,15 @@ const websiteController = (app) => {
     app.get('/', (req, res) => {
         res.sendFile('index.html', { root: path.join("public", '../static') });
     });
-    app.post('/playlists')
+    app.get('/playlists', function (req, res) {
+        conn.query('SELECT * FROM playlists;', function (err, row) {
+            if (err) {
+                return err;
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.send(row);
+        });
+    });
 }
 module.exports = {
     websiteController,
